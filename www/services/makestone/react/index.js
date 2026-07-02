966dca6889da8b428e2fecaa1bd7124e4a4bf8bd
”Vovan” <plategi@yandex.ru>
2026-07-02 19:54:33 +0300

    add page of react testing

---
+++ www/services/makestone/react/index.js
@@ -0,0 +1,13 @@
+		function MyComponent() {
+            const greeting = "Привет Вован это тест JSX";
+            return (
+                <div className="container">
+                    <h1>{greeting}</h1>
+                    <p>This is JSX running directly inside an HTML file.</p>
+                </div>
+            );
+        }
+        // Render the component into the DOM container
+        const container = document.getElementById('root');
+        const root = ReactDOM.createRoot(container);
+        root.render(<MyComponent />);
