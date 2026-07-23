9e3d44a57aa6a18f19192c37add76ec77c5b5b59
”Vovan” <plategi@yandex.ru>
2026-07-23 10:53:56 +0300

    api

--- www/api/post/order/makestone/index.php
+++ www/api/post/order/makestone/index.php
@@ -1,3 +1,29 @@
 <?php
-phpinfo();
+header("Content-Type: application/json; charset=UTF-8");
+//header("Access-Control-Allow-Origin: *"); // Adjust for security as needed
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$inputJSON = file_get_contents('php://input');
+$data = json_decode($inputJSON, true);
+
+if (json_last_error() === JSON_ERROR_NONE) {
+    
+    $name = isset($data['name']) ? $data['name'] : 'Guest';
+	$surname = isset($data['surname']) ? $data['surname'] : 'GuestSur';
+
+    // Build the response
+    $response = array(
+        "status" => "success",
+        'message' => 'Hello, ' . htmlspecialchars($name),
+		'age' => 'Hello, ' . htmlspecialchars($surname)
+    );
+} else {
+    // Handle invalid JSON payload
+    $response = array(
+        'status' => 'error',
+        'message' => 'Invalid JSON received'
+    );
+}
+echo json_encode($response);
 ?>
